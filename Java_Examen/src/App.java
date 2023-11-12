4c9bebd2dad0d540d2db77268b0f554dcf4e0558
ThaisAlvarenga <ata381@nyu.edu>
2023-11-12 00:09:01 -0600

    Added exam project

---
+++ Java_Examen/src/App.java
@@ -0,0 +1,39 @@
+/*
+ * Write description of class
+ * 
+ * @ Thaís Alvarenga
+ * @version 12/10/11
+ */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String s = "";
+        String[] mountains = {"Chimborazo","Matterhorn", "Olympus", "Everest", "Kinabalu", "Adriondack", "Blue Ridge", "Zugspitze", };
+
+        // for (int i = 0; i < mountains.length ; i++)
+        // {
+        //     s+= mountains[i].charAt(i);
+        // }
+
+        // System.out.println(s.substring(2,4));
+
+        // String s1 ="\"\\\\\"";
+        // System.out.println(s1);
+
+        int j=7,k=5,m=8,result; result=j/m*k; 
+        System.out.println(result);
+
+        int array[][] = {{4,5,6}, {7,8,9}};
+
+        for(int i = 0; i<2; i++)
+        for (int j = 0; j<3; j++)
+        System.out.println(array[1][0];)
+
+
+
+
+    }
+}
+
